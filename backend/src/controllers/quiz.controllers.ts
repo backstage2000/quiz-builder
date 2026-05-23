dda5e5b3cfd9ea82971bc7f6bd57735471401c55
Anatoliy Korotenko <tolik.korotenko21@gmail.com>
2026-05-23 09:24:36 +0300

    update controller

--- backend/src/controllers/quiz.controllers.ts
+++ backend/src/controllers/quiz.controllers.ts
@@ -5,10 +5,6 @@ import { ApiError } from '../exeptions/api.error';
 const get = async (_: Request, res: Response): Promise<void> => {
   const quizzes = await quizServices.getAll();
 
-  if (!quizzes.length) {
-    throw ApiError.notFound('Quizzes not found');
-  }
-
   res.json(quizzes);
 };
 
